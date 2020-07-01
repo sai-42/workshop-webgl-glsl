4b7cb3f246656bfb8cac36dc865e0d662662c078
Sabrina Moraru <sabrina.moraru@gmail.com>
2020-07-01 16:04:18 +0300

    adding GLSL noise

--- shader.js
+++ shader.js
@@ -5,6 +5,7 @@ global.THREE = require("three");
 require("three/examples/js/controls/OrbitControls");
 
 const canvasSketch = require("canvas-sketch");
+const glsl = require("glslify");
 
 const settings = {
   // Make the loop animated
@@ -44,7 +45,8 @@ const sketch = ({ context }) => {
     }
   `;
 
-  const fragmentShader = /* glsl */ `
+  const fragmentShader = glsl(/* glsl */ `
+    #pragma glslify: noise = require('glsl-noise/simplex/3d');
     varying vec2 vUv;
     uniform vec3 color;
     uniform float time;
@@ -53,11 +55,15 @@ const sketch = ({ context }) => {
 
       vec2 q = vUv;
       q.x *= 2.0;
-      vec2 pos = mod(q * 5.0, 1.0);
+      vec2 pos = mod(q * 10.0, 1.0);
 
       float d = distance(pos, center);
 
-      float mask = step(0.25 + sin(time + vUv.x * 2.0) * 0.25, d);
+      // float mask = step(0.25 + sin(time + vUv.x * 2.0) * 0.25, d);
+
+      vec2 noiseInput = floor(q * 10.0);
+      float offset = noise(vec3(noiseInput.xy, time)) * 0.25;
+      float mask = step(0.25 + offset, d);
 
       mask = 1.0 - mask;
 
@@ -65,7 +71,7 @@ const sketch = ({ context }) => {
 
       gl_FragColor = vec4(vec3(fragColor), 1.0);
     }
-  `;
+  `);
 
   // Setup a material
   const material = new THREE.ShaderMaterial({
