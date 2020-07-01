f432a8e92e68db72f9e2bc21a0be69800c7e7a84
Sabrina Moraru <sabrina.moraru@gmail.com>
2020-07-01 14:36:04 +0300

    passing data into shaders

--- shader.js
+++ shader.js
@@ -46,14 +46,17 @@ const sketch = ({ context }) => {
 
   const fragmentShader = /* glsl */ `
     varying vec2 vUv;
-    varying vec3 color;
+    uniform vec3 color;
     void main () {
-      gl_FragColor = vec4(vec3(vUv.x), 1.0);
+      gl_FragColor = vec4(vec3(vUv.x) * color, 1.0);
     }
   `;
 
   // Setup a material
   const material = new THREE.ShaderMaterial({
+    uniforms: {
+      color: { value: new THREE.Color("#ffr00ff") },
+    },
     vertexShader,
     fragmentShader,
   });
